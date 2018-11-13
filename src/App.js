39e2dd7d18a8c29bf55e1922ca076476c5f55d06
SparksFullStack <csparks@antioch.edu>
2018-11-12 17:26:17 -0800

    basics of the header section completed

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+import './App.css';
+
+import Header from './components/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Header />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Nav, NavItem, NavLink } from 'reactstrap';
+import './Header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-md navbar-light fixed-top py-4 color--p-blue">
+        <div className="container">
+            <a href='#' className="navbar-brand">
+                <h3 className="d-inline align-middle color--white">Noah Bartell</h3>
+            </a>
+
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a href="#home" className="nav-link link--highlight">Home</a>
+                    </li>
+                    <li className="nav-item ">
+                        <a href="#about" className="nav-link link--highlight">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#films" className="nav-link link--highlight">Filmography</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+      </nav>
+    )
+  }
+}
